bab71ce6c50fe94a4a0513d5cdb7fa17e1c16195
jan kuwert <jan.kuwert@gmx.de>
2025-01-08 17:45:27 +0100

    make mobile detail dialog full screen instead of drawer

--- frontend/src/components/responsive-dialog.tsx
+++ frontend/src/components/responsive-dialog.tsx
@@ -7,13 +7,6 @@ import {
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog'
-import {
-  Drawer,
-  DrawerContent,
-  DrawerHeader,
-  DrawerTitle,
-} from '@/components/ui/drawer'
-import { useBreakpoints } from '@/lib/breakpoints'
 
 interface ResponsiveDialogProps {
   children: React.ReactNode
@@ -32,35 +25,17 @@ export function ResponsiveDialog({
   description,
   large = false,
 }: ResponsiveDialogProps) {
-  const { sm } = useBreakpoints()
-
-  if (sm) {
-    return (
-      <Dialog open={isOpen} onOpenChange={setIsOpen}>
-        <DialogContent
-          className={`max-h-[98vh] overflow-scroll sm:max-w-[425px] md:max-w-[600px] lg:max-w-[750px] ${large && '[&_button:not(.exclude)]:absolute [&_button:not(.exclude)]:right-7 [&_button:not(.exclude)]:top-7 [&_button:not(.exclude)]:z-10 [&_button:not(.exclude)]:rounded-full [&_button:not(.exclude)]:bg-background [&_button:not(.exclude)]:p-2 [&_button:not(.exclude)]:text-foreground [&_div.flex.text-center]:hidden [&_svg]:size-5'}`}
-        >
-          <DialogHeader>
-            <DialogTitle>{title}</DialogTitle>
-            {description && (
-              <DialogDescription>{description}</DialogDescription>
-            )}
-          </DialogHeader>
-          {children}
-        </DialogContent>
-      </Dialog>
-    )
-  }
-
   return (
-    <Drawer open={isOpen} onOpenChange={setIsOpen}>
-      <DrawerContent className="mb-5 px-4">
-        <DrawerHeader className="text-left">
-          <DrawerTitle>{title}</DrawerTitle>
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogContent
+        className={`h-[98vh] max-h-[98vh] max-w-[98vw] overflow-scroll rounded-md p-4 sm:p-6 md:max-w-[600px] lg:max-w-[750px] ${large && '[&_button:not(.exclude)]:absolute [&_button:not(.exclude)]:right-5 [&_button:not(.exclude)]:top-5 [&_button:not(.exclude)]:z-10 [&_button:not(.exclude)]:rounded-full [&_button:not(.exclude)]:bg-background/50 [&_button:not(.exclude)]:p-2 [&_button:not(.exclude)]:text-foreground [&_button:not(.exclude)]:backdrop-blur-sm [&_button:not(.exclude)]:hover:bg-background/90 [&_button:not(.exclude)]:sm:right-7 [&_button:not(.exclude)]:sm:top-7 [&_div.flex.text-center]:hidden [&_svg]:size-5'}`}
+      >
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
           {description && <DialogDescription>{description}</DialogDescription>}
-        </DrawerHeader>
+        </DialogHeader>
         {children}
-      </DrawerContent>
-    </Drawer>
+      </DialogContent>
+    </Dialog>
   )
 }
